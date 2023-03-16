684d610686ee2ab8dd94df4e8d8f37a7a003fcbd
Ryan Nitz <ryan.nitz@coinbase.com>
2023-03-15 20:15:16 -0400

    Change name

--- proxy/handlers/credentials.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-import "net/http"
-
-func RetrieveCredentials(w http.ResponseWriter, r *http.Request) {
-
-}
--- proxy/model.go
+++
@@ -1,6 +0,0 @@
-package proxy
-
-type CreatePoolRequest struct {
-	DisplayName string `json:"displayName"`
-	PoolId      string `json:"poolId"`
-}
